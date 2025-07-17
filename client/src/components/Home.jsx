208dd10f610b84ac1964f27ae49e56353460fbbd
ayamte <ayamtejjal123@gmail.com>
2025-07-17 15:53:40 +0100

    dockerfile frontend

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';  
+import { Link } from 'react-router-dom';  
+  
+const Home = () => {  
+  return (  
+    <div className="home">  
+      <h1>Bienvenue sur ChronoGaz</h1>  
+      <p>Application Frontend React</p>  
+      <div>  
+        <p>Hello</p>
+      </div>  
+    </div>  
+  );  
+};  
+  
+export default Home;
